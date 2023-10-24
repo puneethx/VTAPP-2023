40e587abbbc98ba86ec435c7d154888b2798dd78
Rion Dsilva <118284932+RionDsilvaCS@users.noreply.github.com>
2023-10-24 22:58:25 +0530

    hero page update

--- src/Components/HeroFolder/Hero.jsx
+++ src/Components/HeroFolder/Hero.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './hero.scss'
+import vtappLogo from '../../assets/old_vtapp_logo.svg'
 
 const Hero = () => {
+
   return (
-    <div>Hero</div>
+    <>
+    <div className='hero-back'>
+
+      <div className='hero-top'>
+        <img src={vtappLogo}/><div />
+      </div>
+      <div className='hero-center'>
+        <center><h1>vTAPP<br/>2023</h1></center>
+        <center><p>COMMING SOON</p></center>
+      </div>
+      <div className='hero-bottom'>
+        <div /><p>9<sup>th</sup> & 10<sup>th</sup> December</p>
+      </div>
+    </div>
+    </>
   )
 }
 
